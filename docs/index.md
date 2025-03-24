e2450b52fd918587cd978dec78212d2a7cf06e8d
Scaffolder <scaffolder@backstage.io>
2025-03-24 10:43:22 +0000

    initial commit

---
+++ docs/index.md
@@ -0,0 +1,11 @@
+# Confluent Cloud Environment
+
+This is the documentation for your Confluent Cloud environment.
+
+## Overview
+
+This environment was created using Backstage and Terraform. The environment is managed through Infrastructure as Code, ensuring consistency and repeatability.
+
+## Details
+
+See the [Environment Details](./environment-details.md) page for specific information about this environment.
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = ">= 0.2.0"
+    }
+  }
+}
+
+provider "confluent" {
+  # The provider will automatically use CONFLUENT_CLOUD_API_KEY and CONFLUENT_CLOUD_API_SECRET
+  # environment variables without explicit configuration
+}
+
+variable "environment_name" {
+  type    = string
+  default = "test3"
+}
+
+resource "confluent_environment" "this" {
+  display_name = var.environment_name
+}
+
+# Add outputs to be used in documentation
+output "environment_id" {
+  value = confluent_environment.this.id
+  description = "The ID of the created Confluent Cloud environment"
+}
+
+output "environment_name" {
+  value = confluent_environment.this.display_name
+  description = "The name of the created Confluent Cloud environment"
+}
